95bc73a9bc0eb86283daf2832e65a2d4df95b0ef
PietroMarilei <pietromrl0@gmail.com>
2024-02-11 16:13:21 +0100

    //added a google sign in need to generate jwt

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -46,4 +46,46 @@ router.post("/logout", (req: Request, res: Response) => {
   res.json("Logged out");
 });
 
+//-----------------google
+// src/routes.ts
+import { Router } from "express";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "625912430702-rhj0mfvsrna76rn8njvkv2vi4fig31r1.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Questa è la funzione di callback chiamata dopo l'autenticazione con Google
+      // Puoi gestire il profilo dell'utente qui e autenticarlo nel tuo sistema
+      // Ad esempio, puoi cercare o creare un utente nel tuo database e autenticarlo
+      
+      // 👍️ qui devo verificare se l'utente é nel db e generare il jwt per lui
+      console.log("auth google success!", profile);
+      return done(null, profile);
+    }
+  )
+);
+
+// Rotte per l'autenticazione con Google
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    // Successo dell'autenticazione
+    res.redirect("/");
+  }
+);
+
+
 export default router;
--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
+import cors from "cors";
+
 
 const app = express();
 const port = 3000;
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // ciclo per scrivere dinamicamente i controller
